cc4d3cb24f1e59cf704b89aa6ad4ca3c9a0c9974
{o} Studio <brain@roswell.pl>
2018-05-26 00:17:34 +0200

    Lexer

--- spedn/library/Lexer.hs
+++ spedn/library/Lexer.hs
@@ -1,1 +1,48 @@
 module Lexer where
+
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void String
+
+spaceConsumer :: Parser ()
+spaceConsumer = L.space space1 line block
+  where
+    line = L.skipLineComment "///"
+    block = L.skipBlockComment "/*" "*/"
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme spaceConsumer
+
+symbol :: String -> Parser String
+symbol = L.symbol spaceConsumer
+
+num :: Parser Integer
+num = lexeme L.decimal
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+brackets :: Parser a -> Parser a
+brackets = between (symbol "[") (symbol "]")
+
+braces :: Parser a -> Parser a
+braces = between (symbol "{") (symbol "}")
+
+semi :: Parser String
+semi = symbol ";"
+
+keyword :: String -> Parser ()
+keyword w = lexeme . try $ string w *> notFollowedBy alphaNumChar
+
+keywords :: [String]
+keywords = ["contract","challenge","if","then","else","verify","true","false"]
+
+name :: Parser String
+name = lexeme . try $ do
+    word <- (:) <$> letterChar <*> many alphaNumChar
+    if elem word keywords
+      then fail $ "keyword " ++ show word ++ " cannot be an identifier"
+      else return word
